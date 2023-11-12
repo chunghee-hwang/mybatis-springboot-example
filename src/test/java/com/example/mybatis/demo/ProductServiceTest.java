c4c70a130a4cc13ece320c5427eb73a4f781594c
Chunghee Hwang <hch0821@gmail.com>
2023-11-12 23:14:12 +0900

    refactor: SpringBootTest => MybatisTest로 변경 Spring Test보다 더 가볍도록 변경

--- src/test/java/com/example/mybatis/demo/ProductServiceTest.java
+++ src/test/java/com/example/mybatis/demo/ProductServiceTest.java
@@ -1,58 +1,70 @@
 package com.example.mybatis.demo;
 
 import com.example.mybatis.demo.model.Product;
-import com.example.mybatis.demo.service.ProductService;
-import lombok.RequiredArgsConstructor;
+import com.example.mybatis.demo.repository.ProductMapper;
 import lombok.extern.slf4j.Slf4j;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Slf4j
-@SpringBootTest
+@MybatisTest
 public class ProductServiceTest {
+    private final ProductMapper productMapper;
+
     @Autowired
-    private ProductService productService;
+    public ProductServiceTest(ProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
 
     @Test
     public void getProductById() {
-        Product product = productService.getProductById(1L);
+        Product product = productMapper.selectProductById(1L);
         log.info("product: {}", product);
+        assertNotNull(product);
     }
 
     @Test
     public void getAllProducts() {
-        List<Product> products = productService.getAllProducts();
+        List<Product> products = productMapper.selectAllProducts();
         log.info("products: {}", products);
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
     }
 
     @Transactional
     @Test
     public void addProduct() {
-        productService.addProduct(
+        List<Product> prevProducts = productMapper.selectAllProducts();
+        int prevSize = prevProducts.size();
+        productMapper.insertProduct(
                 Product
                         .builder()
                         .prodName("쿤달 샴푸")
                         .prodPrice(7900)
                         .build()
         );
-        productService.addProduct(
+        productMapper.insertProduct(
                 Product
                         .builder()
                         .prodName("마스크팩")
                         .prodPrice(1000)
                         .build()
         );
-        productService.addProduct(
+        productMapper.insertProduct(
                 Product
                         .builder()
                         .prodName("티셔츠")
                         .prodPrice(5900)
                         .build()
         );
+        List<Product> curProducts = productMapper.selectAllProducts();
+        int curSize = curProducts.size();
+        assertEquals(prevSize + 3, curSize);
     }
 }
